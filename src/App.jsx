8c7a1f2ca14a7826f69db8334ae2602b1b684d21
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2025-08-19 23:08:18 -0400

    new destinations, spaces adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,11 @@ function App() {
   return (
     <>
      <Header/>
-     <main className='grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-8
-                      p-4 
+     <main className='grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-18
+                      p-12 
                       bg-[#f7edd2] 
                       lg:p-12'>
+                        <h2 className='col-span-full mx-auto text-2xl font-bold text-stone-800'>Our destinations</h2>
       {destinations.map(destination => (
          <Card key={destination.id} destination ={destination}/>
       ))}
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,13 +2,13 @@ export default function Card({ destination }) {
 
   const { name, country, continent, description, image } = destination
   return (
-    <article className="flex flex-col items-center gap-4 
+    <article className="flex flex-col items-start gap-4 
                         border-2 border-[#f5cac3] shadow-sm shadow-red-700 rounded-sm 
                         bg-[#f28482]">
       <img src={image} alt={name} className="w-full aspect-[1.3/1] object-cover" />
-      <div className="flex flex-col gap-4 items-center p-4">
-        <h2 className="font-semibold text-2xl">{name}</h2>
-        <p className=" text-lg"><span className="font-medium">Country:</span> {country}</p>
+      <div className="flex flex-col gap-2 items-start justify-between p-4 h-full">
+        <h2 className="font-semibold text-2xl text-center self-center mb-4">{name}</h2>
+        <p className=" text-lg mt-auto"><span className="font-medium">Country:</span> {country}</p>
         <p className=" text-lg"><span className="font-medium">Continent:</span> {continent}</p>
         <p className=" text-lg"><span className="font-medium">Description:</span> {description}</p>
       </div>
