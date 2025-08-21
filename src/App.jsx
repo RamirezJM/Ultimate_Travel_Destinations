e2221d0ab07272d71e8a80183768ea5a4cb391a5
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2025-08-20 21:17:44 -0400

    color change

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
                       p-12 
                       bg-[#f7edd2] 
                       lg:p-12'>
-                        <h2 className='col-span-full mx-auto text-2xl font-bold text-stone-800'>Our destinations</h2>
+                        <h2 className='col-span-full mx-auto text-2xl font-bold font-serif text-stone-800'>Our destinations</h2>
       {destinations.map(destination => (
          <Card key={destination.id} destination ={destination}/>
       ))}
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,11 +3,10 @@ export default function Card({ destination }) {
   const { name, country, continent, description, image } = destination
   return (
     <article className="flex flex-col items-start gap-4 
-                        border-2 border-[#f5cac3] shadow-sm shadow-red-700 rounded-sm 
-                        bg-[#f28482]">
+                        border-2 border-[#f5cac3] shadow-sm shadow-red-700 rounded-sm">
       <img src={image} alt={name} className="w-full aspect-[1.3/1] object-cover" />
       <div className="flex flex-col gap-2 items-start justify-between p-4 h-full">
-        <h2 className="font-semibold text-2xl text-center self-center mb-4">{name}</h2>
+        <h2 className="font-semibold text-2xl text-center self-center w-full mb-4" >{name}</h2>
         <p className=" text-lg mt-auto"><span className="font-medium">Country:</span> {country}</p>
         <p className=" text-lg"><span className="font-medium">Continent:</span> {continent}</p>
         <p className=" text-lg"><span className="font-medium">Description:</span> {description}</p>
